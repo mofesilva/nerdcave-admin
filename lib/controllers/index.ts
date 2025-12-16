9e2ee02f8f4983a70163c4bed805e11628c653aa
mofesilva <mofesilva.dev@gmail.com>
2025-12-16 12:08:17 -0300

    Add index file to export all controllers for easier access

---
+++ lib/controllers/index.ts
@@ -0,0 +1,8 @@
+export { LinksController } from './Links.controller';
+export { ProfileController } from './Profile.controller';
+export { AnalyticsController } from './Analytics.controller';
+export { ArticlesController } from './Articles.controller';
+export { AlbumsController } from './Albums.controller';
+export { CategoriesController } from './Categories.controller';
+export { TagsController } from './Tags.controller';
+export { MediaController } from './Media.controller';
