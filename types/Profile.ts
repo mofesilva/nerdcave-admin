ff57b4430316687805bd0cf7b4469a486b908626
mofesilva <mofesilva.dev@gmail.com>
2025-12-16 12:05:52 -0300

    Add Profile interface definition for type safety

---
+++ types/Profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    _id: string;
+    name: string;
+    title: string;
+    bio: string;
+    followers: number;
+    videos: number;
+    views: number;
+    updatedAt: Date;
+}
