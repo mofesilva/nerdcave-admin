76de2af0ff73c6d53b7fc036b00ad611d96152fe
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:38:09 -0300

    feat: add Media mapper for converting document fields to Media model

---
+++ lib/medias/Media.mapper.ts
@@ -0,0 +1,15 @@
+import { Media } from './Media.model';
+import { toCamelCaseKeys } from '../utils';
+
+export function mediaFromDocument(doc: any): Media {
+    // Converte todas as chaves de snake_case para camelCase recursivamente
+    const data = toCamelCaseKeys(doc) as any;
+
+    return {
+        _id: data._id ?? '',
+        fileName: data.fileName ?? '',
+        title: data.title ?? data.originalName ?? '',
+        altText: data.altText ?? data.originalName ?? '',
+        deleted: data.deleted ?? false,
+    };
+}
