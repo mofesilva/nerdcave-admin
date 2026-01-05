584095541e5342d50618e259e46a26a89f64ba16
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:40:26 -0300

    feat: add Settings interface for application configuration

---
+++ types/Settings.ts
@@ -0,0 +1,12 @@
+export interface Settings {
+    _id: string;
+    primaryColor: string;
+    secondaryColor?: string;
+    accentColor?: string;
+    cardColor?: string;
+    backgroundColor?: string;
+    backgroundImgUrl?: string;
+    siteName?: string;
+    siteDescription?: string;
+    siteKeywords?: string;
+}
