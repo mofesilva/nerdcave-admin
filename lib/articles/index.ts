987f15665eb5078b282c696831c0c8090e4d5f9c
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:35:36 -0300

    feat: create index file for articles module to export related components

---
+++ lib/articles/index.ts
@@ -0,0 +1,5 @@
+export * from './Article.model';
+export * from './Article.mapper';
+export * from './Articles.collection';
+export * from './Article.service';
+export * as ArticlesController from './Article.controller';
