25c2900ee42136edc90f14053a528335b2c79226
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:35:13 -0300

    feat: create index file for analytics module exports

---
+++ lib/analytics/index.ts
@@ -0,0 +1,5 @@
+export * from './Analytic.model';
+export * from './Analytic.mapper';
+export * from './Analytics.collection';
+export * from './Analytic.service';
+export * as AnalyticsController from './Analytic.controller';
