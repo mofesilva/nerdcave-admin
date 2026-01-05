d7cecaa121ada2028fd522ef0569af3c0214ca43
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:36:44 -0300

    feat: create index file for category module exports

---
+++ lib/categories/index.ts
@@ -0,0 +1,5 @@
+export * from './Category.model';
+export * from './Category.mapper';
+export * from './Categories.collection';
+export * from './Category.service';
+export * as CategoriesController from './Category.controller';
