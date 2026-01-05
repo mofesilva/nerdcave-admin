eebc2f1a0656a3106160c0455fd8a617ae1a14a8
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:38:03 -0300

    feat: create index file for Media module exports

---
+++ lib/medias/index.ts
@@ -0,0 +1,5 @@
+export * from './Media.model';
+export * from './Media.mapper';
+export * from './Medias.collection';
+export * from './Media.service';
+export * as MediaController from './Media.controller';
