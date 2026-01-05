5935708508719bc507cf364f67e14829603338be
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:37:45 -0300

    feat: create index file for links module exports

---
+++ lib/links/index.ts
@@ -0,0 +1,5 @@
+export * from './Link.model';
+export * from './Link.mapper';
+export * from './Links.collection';
+export * from './Link.service';
+export * as LinksController from './Link.controller';
