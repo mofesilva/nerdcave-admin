148504107e030dc2a4f3e20218b64e7b5105d6fc
mofesilva <mofesilva.dev@gmail.com>
2026-01-05 08:34:55 -0300

    feat: create index file for Albums module to export related components

---
+++ lib/albums/index.ts
@@ -0,0 +1,5 @@
+export * from './Album.model';
+export * from './Album.mapper';
+export * from './Albums.collection';
+export * from './Album.service';
+export * as AlbumsController from './Album.controller';
