212045ec93325d528ca2e4b4765c09fdc64a9df8
mofesilva <mofesilva.dev@gmail.com>
2026-01-15 22:39:01 -0300

    feat: add index file for exporting theme components

---
+++ app/admin/theme/_components/index.ts
@@ -0,0 +1,4 @@
+export { default as AccentColorSection } from "./AccentColorSection";
+export { default as LogosSection } from "./LogosSection";
+export { default as LogoUploadCard } from "./LogoUploadCard";
+export { default as BlogSettingsTab } from "./BlogSettingsTab";
