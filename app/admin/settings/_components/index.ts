0b6acaeb81477fc14ca424adbf061ce61d2824a2
mofesilva <mofesilva.dev@gmail.com>
2026-01-15 22:06:29 -0300

    feat: add index file for exporting admin settings components

---
+++ app/admin/settings/_components/index.ts
@@ -0,0 +1,6 @@
+export { default as AccentColorSection } from "./AccentColorSection";
+export { default as AccentTextColorSection } from "./AccentTextColorSection";
+export { default as BlogInfoSection } from "./BlogInfoSection";
+export { default as LogosSection } from "./LogosSection";
+export { default as LogoUploadCard } from "./LogoUploadCard";
+export { default as BlogSettingsTab } from "./BlogSettingsTab";
