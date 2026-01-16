0f8b5c6a6b344ba61dd15324d663e1c79b2bf6ca
mofesilva <mofesilva.dev@gmail.com>
2026-01-15 22:40:25 -0300

    fix: correct import statement for Settings model in Settings.collection.ts

--- lib/settings/Settings.collection.ts
+++ lib/settings/Settings.collection.ts
@@ -1,6 +1,7 @@
-"use client";
+'use client';
+
 import { createCollection } from '@/lib/cappuccino/client';
-import type { Settings } from './Settings.model';
+import { Settings } from './Settings.model';
 
 export function getSettingsCollection() {
     return createCollection<Settings>('settings');
