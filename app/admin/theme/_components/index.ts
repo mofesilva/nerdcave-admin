2121514f6e7fd87f1adb7a8871f01cffad63d45d
mofesilva <mofesilva.dev@gmail.com>
2026-01-17 09:12:59 -0300

    feat: add ThemeColorsSection to admin theme components

--- app/admin/theme/_components/index.ts
+++ app/admin/theme/_components/index.ts
@@ -2,3 +2,4 @@ export { default as AccentColorSection } from "./AccentColorSection";
 export { default as LogosSection } from "./LogosSection";
 export { default as LogoUploadCard } from "./LogoUploadCard";
 export { default as BlogSettingsTab } from "./BlogSettingsTab";
+export { default as ThemeColorsSection } from "./ThemeColorsSection";
